0b31d257d1bc8b15a0ac6a221400386418b758a8
Justin Green <justin98366@gmail.com>
2017-06-13 10:27:42 -0700

    fixing a small error

--- lab-justin/README.md
+++ lab-justin/README.md
@@ -1,3 +1,4 @@
 # README
 
 This is a project to practice some Node.js functionality and making synchronous tests using Mocha.js
+A total of 6 tests are written for a greet function and an arithmetic function.
--- lab-justin/test/greet-test.js
+++ lab-justin/test/greet-test.js
@@ -4,9 +4,9 @@ const expect = require('expect');
 const greet = require('../lib/greet.js');
 
 describe('greet', () => {
-  it('should return Hello, Justin', () => {
-    let result = greet('Justin');
-    expect(result).toEqual('Hello, Justin');
+  it('should return Hello, world!', () => {
+    let result = greet('world!');
+    expect(result).toEqual('Hello, world!');
   });
   it('should return null', () => {
     let result = greet(7);
